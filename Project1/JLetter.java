7b5c93902f2cd43842041fdf5f5d8386b8e8a286
Andrew Moon <157635268+taehomoo@users.noreply.github.com>
2024-08-20 23:43:49 -0500

    Add files via upload

---
+++ Project1/JLetter.java
@@ -0,0 +1,28 @@
+/**
+*
+* Prints the letter J composed of letters 'J','A','V','A'.
+* Project 1
+* @author Andrew Moon - COMP1210 - 006
+* @version 8/24/22
+*/
+
+public class JLetter {
+
+   /** Prints letters J, A, and V in the shape of a letter J.
+   *
+   * @param args Command line arguments (not used).
+   */
+
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("   JAVAJA");
+   }
+}
---
+++ Project1/MyLifeGoals.java
@@ -0,0 +1,27 @@
+/**
+*
+* Prints my Life Goals.
+* Project 1
+* @author Andrew Moon - COMP1210 - 006
+* @version 8/22/22
+*/
+
+public class MyLifeGoals {
+
+   /** Prints short-term, medium-term, and long-term life goals.
+   *
+   * @param args Command line arguments (not used).
+   */
+   
+   public static void main(String[] args) {
+      System.out.println("Andrew Moon");
+      System.out.println();
+      System.out.println("My short-term goals in life are to "
+         + "get an A in all my classes this semester");
+      System.out.println("My medium-term goals in life are to "
+         + "graduate college with work experience and a good GPA");
+      System.out.println("My long-term goals in life are to "
+         + "work a stable job and have a family");
+   }
+
+}
